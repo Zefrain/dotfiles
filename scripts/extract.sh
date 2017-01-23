e79c96c560a90143081d52ba47ab2227d61048eb
zefrain <whiothes81@gmail.com>
2017-01-23 09:25:54 +0800

    feat: Add extract.sh && clang_format.sh

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,15 +1,26 @@
+#!/usr/bin/bash
+
+
+flist=$(find . -iname '*.rar' -o -iname '*.zip');
 
 OLD=$IFS
 IFS=$'\n'
 
-filelist=$(find . -iname '*.rar' -o -iname '*.zip')
-
-for file in $filelist
+for fname in $flist
 do
-        echo $file
-        fpath=$(dirname "$file")
-        unar -e cp936 "$file" -o "$fpath" -s
-        rm "$file"
+    oldname=$fname
+    echo "oldname=$oldname"
+    fname=$(ls $oldname| perl -p -e "s|\.\d\.|.|g")
+    mv $oldname $fname
+    echo "fname=$fname"
+
+    dname=${fname#*\ }
+    dname=${dname%\.*}
+    echo "dname=$dname"
+    outdir="$(dirname "$file")/$dname"
+    echo "outdir=$outdir"
+    rm -rf $outdir && mkdir $outdir
+    7z -aoa x $fname -o$outdir && rm $fname
 done
 
 IFS=$OLD
