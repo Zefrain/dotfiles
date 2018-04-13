b11b6af59c8582522716c89798c51c3c479f7fac
zefrain <whiothes81@gmail.com>
2018-04-13 11:25:26 +0800

    change .spacemacs

--- exec/extract.sh
+++ exec/extract.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 
-flist=$(find . -iname '*.rar' -o -iname '*.7z');
+# flist=$(find . -iname '*.rar' -o -iname '*.7z');
+flist=$(ag -g '\.(rar|7z)$')
+# dlist=$(ag -g '\(1\)\.(rar|7z)$')
+dlist=$(find . -type d -name '*\(1\)')
 
 OLD=$IFS
 IFS=$'\n'
@@ -10,17 +13,36 @@ for fname in $flist
 do
     oldname=$fname
     echo "oldname=$oldname"
-    fname=$(ls $oldname| perl -p -e "s|\.\d\.|.|g")
+    # fname=$(ls $oldname| perl -p -e "s|\.\d\.|.|g")
+    fname=${fname/\(1\)}
     mv $oldname $fname
     echo "fname=$fname"
 
-    # dname=${fname#*\ }
     dname=${fname%\.*}
     echo "dname=$dname"
     outdir="$(dirname "$file")/$dname"
     echo "outdir=$outdir"
+
     rm -rf $outdir && mkdir $outdir
     7z -aoa x $fname -o$outdir && rm $fname
+
+    if [ $? != 0 ] ; then
+        rm -rf "$outdir"
+
+        if [ -d "${outdir}\(1\)" ] ; then
+            rm $fname; mv "${outdir}\(1\)" "$outdir"
+        fi
+    fi
 done
+
+# for dname in $dlist
+# do
+#     dst=${dname/\(1\)/}
+#     rm -rf "$dst"
+#     echo $dname
+#     echo $dst
+#     mv "$dname" "$dst"
+# done
+
 
 IFS=$OLD
