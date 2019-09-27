13a5e58845924c7f3703ab163071d9c9c6bbb53a
zefrain <whiothes81@gmail.com>
2019-09-27 13:44:33 +0800

    chore: update spacemacs

--- conf/emacs/.spacemacs.d/load.d/hooks.el
+++ conf/emacs/.spacemacs.d/load.d/hooks.el
@@ -1,5 +1,4 @@
 (add-hook 'c-mode-hook   (lambda () (c-toggle-comment-style -1))) ;;
 (add-hook 'sql-mode-hook (lambda () (sql-set-product 'mysql)))
 (add-hook 'lua-mode-hook (lambda () (flycheck-luacheck-standards ' ("ngx_lua"))))
-(add-hook 'org-mode-hook (lambda () (setq org-confirm-babel-evaluate nil
-                                          )))
+(add-hook 'org-mode-hook (lambda () (setq org-confirm-babel-evaluate nil)))
--- conf/emacs/.spacemacs.d/load.d/variable.el
+++ conf/emacs/.spacemacs.d/load.d/variable.el
@@ -1,34 +1,30 @@
-(setq plantuml-jar-path     (concat (shell-command-to-string "echo -ne $(brew --prefix plantuml)") "/libexec/plantuml.jar"))
-(setq org-plantuml-jar-path (concat (shell-command-to-string "echo -ne $(brew --prefix plantuml)") "/libexec/plantuml.jar"))
+;; platnuml
+(with-eval-after-load 'plantuml-mode
+  (setq plantuml-jar-path     (concat (shell-command-to-string "echo -ne $(brew --prefix plantuml)") "/libexec/plantuml.jar"))
+  (setq org-plantuml-jar-path (concat (shell-command-to-string "echo -ne $(brew --prefix plantuml)") "/libexec/plantuml.jar")))
 
 ;; winum
 (setq winum-scope (quote frame-local))
 
-(setq org-export-with-sub-superscripts nil)
 
+;; coding system
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
-
 (prefer-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
 
 ;; backup-file
 (setq backup-directory-alist (quote ((".*" . "~/.spacemacs.d/backup"))))
 ;; (setq make-backup-file-name-function (quote backup-file-name-function-custom))
-(setq org-export-with-sub-superscripts (quote {}))
-
-;; (prefer-coding-system 'gb2312-dos)
-;; (setq default-buffer-file-coding-system 'gb2312-dos)
-
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 
 (configuration-layer/remove-layer 'web-mode)
 
 (setq comment-style 'indent)
-(ignore 'defvaralias)
 
+;; set warning level
+(ignore 'defvaralias)
 
 (setq warning-minimum-level ':error)
 (setq dotspacemacs-mode-line-theme '(all-the-icons :separator 'arrow))
@@ -37,4 +33,25 @@
 ;; line break
 (setq truncate-lines t)
 
+;; vc-control
 (setq vc-follow-symlinks nil)
+
+;; org
+(setq org-export-with-sub-superscripts nil)
+(setq org-export-with-sub-superscripts (quote {}))
+
+;; org-projectile
+(with-eval-after-load 'org-projectile
+  (setq org-projectile-file "doc/TODOs.org"))
+
+;; org-agenda
+(defun push-agenda (file)
+  "push file to org-agenda-files"
+  (when (file-exists-p file)
+    (push file org-agenda-files)))
+
+(with-eval-after-load 'org-agenda
+  (require 'org-projectile)
+  (mapcar 'push-agenda (org-projectile-todo-files))
+  (require 'org-journal)
+  (push-agenda org-journal-dir))
