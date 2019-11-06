05b2c214450aa9431b4eacc434568c7e7ce0d152
zefrain <whiothes81@gmail.com>
2019-11-06 14:33:18 +0800

    refactor: update config files

--- install.sh
+++ install.sh
@@ -27,14 +27,14 @@ init_conf() {
 init_dotfiles() {
     git submodule update --init --recursive
 
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        proxy brew tap homebrew/command-not-found mycli
+    fi
+
     symlinks -d $HOME
 
     init_sh
     init_conf
-
-    if [[ "$(uname -s)" == "Darwin" ]]; then
-        proxy brew tap homebrew/command-not-found mycli
-    fi
 }
 
 
--- sh/stop.sh
+++ sh/stop.sh
@@ -1,5 +1,13 @@
-if [ ! -z $1 ]; then
-    pgrep $1 | xargs -n 1 kill -9 || ps -ef | grep $1 | grep -v "grep" | awk '{system("kill -9 " $2)}' || lsof -nPi | grep $1 | awk '{print "kill -9", $2}' | sh
+if [ ! -z $# ]; then
+    echo "pgrep $* | xargs -n 1 kill -9"
+    # || ps -ef | grep $* | grep -v "grep" | awk '{system("kill -9 " $2)}' || lsof -nPi | grep $* | awk '{print "kill -9", $2}' | sh
 else
     echo "please give argument to kill";
+fi
+
+
+if [ $? -eq 0 ]; then
+    echo "success"
+else
+    echo "failed"
 fi
