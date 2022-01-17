67b91edbaacf0bbe0b428bbebb0d73dd05993153
zefrain <whiothes81@gmail.com>
2022-01-17 14:32:28 +0800

    feat: add clash config

--- install.sh
+++ install.sh
@@ -18,6 +18,8 @@ init_sh() {
 }
 
 init_conf() {
+	mkdir -p ~/.config/clash
+	stow -d $conf_dir -t ~/.config/clash clash
 	stow -d $conf_dir -t $HOME -R emacs
 	stow -d $conf_dir -t $HOME -R zsh
 	stow -d $conf_dir -t $HOME -R aria2
