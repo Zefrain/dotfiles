b11edacf36192fe56e0d6a2e4abcab2ecc853a2f
zefrain <whiothes81@gmail.com>
2023-06-19 11:03:53 +0800

    feat: add snapclean for ubuntu

--- sh/ch_user.sh
+++ sh/ch_user.sh
@@ -7,17 +7,9 @@ read -p 'CORRECT_EMAIL:' CORRECT_EMAIL
 
 git filter-branch --force --env-filter '
 
-OLD_EMAIL=$OLD_EMAIL
-CORRECT_NAME=$CORRECT_NAME
-CORRECT_EMAIL=$CORRECT_EMAIL
-
-# OLD_EMAIL="whiothes81@gmail.com"
-# CORRECT_NAME="周尚"
-# CORRECT_EMAIL="zhou_s@xingdata.com"
-
-# OLD_EMAIL="zhou_s@xingdata.com"
-# CORRECT_NAME="whiothes"
-# CORRECT_EMAIL="whiothes81@gmail.com"
+OLD_EMAIL="$OLD_EMAIL"
+CORRECT_NAME="$CORRECT_NAME"
+CORRECT_EMAIL="$CORRECT_EMAIL"
 
 # if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 if [ "GIT_COMMITER_NAME" = "$OLD_NAME" ]
