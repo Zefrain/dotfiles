42878fa6792fe8bd6988ad92d84e1aa7020a2ba6
zefrain <whiothes81@gmail.com>
2024-11-18 15:56:51 +0800

    refactor: Update install.sh

--- install.sh
+++ install.sh
@@ -1,78 +1,72 @@
 #!/bin/bash
 
-dotfiles_dir=$(dirname $(realpath $0))
+set -euxo pipefail
 
+# Define directories
+dotfiles_dir="$(dirname "$(realpath "$0")")"
 conf_dir="$dotfiles_dir/conf"
 zsh_dir="$conf_dir/zsh"
 systemd_dir="$dotfiles_dir/systemd"
 
-
+# Detect platform
 PLATFORM=$(sh sh/systype.sh)
 
+# Get OS release information
 get_os_release() {
-	if [ $PLATFORM == "linux" ] ; then
-		. /etc/os-release
-		OS_RELEASE=${Name}
-	fi
+    if [[ $PLATFORM == "linux" ]]; then
+        source /etc/os-release
+    fi
 }
 
+# Initialize shell scripts
 init_sh() {
-	sudo symlinks -d /usr/local/bin/
-	sudo stow -d $dotfiles_dir -t /usr/local/bin -R sh
+    sudo symlinks -d /usr/local/bin/
+    sudo stow -d "$dotfiles_dir" -t /usr/local/bin -R sh
 }
 
+# Initialize configuration files
 init_conf() {
-	mkdir -p ~/.config/clash
-	# stow -d $conf_dir -t ~/.config/clash clash
-	stow -d $conf_dir -t $HOME -R emacs
-	stow -d $conf_dir -t $HOME -R aria2
-	stow -d $conf_dir -t $HOME -R tmux
+    mkdir -p "$HOME/.config/clash"
+    stow -d "$conf_dir" -t "$HOME" -R emacs aria2 tmux
 }
 
+# Initialize Zsh
 init_zsh() {
-	rm -f ~/.zshrc
-	stow -d $conf_dir -t $HOME -R zsh
-	sed -i -e "s|#\? \?ZSH_CUSTOM=.*|ZSH_CUSTOM=${zsh_dir}\/omz_custom|g" $(realpath $HOME/.zshrc)
+    rm -f "$HOME/.zshrc"
+    stow -d "$conf_dir" -t "$HOME" -R zsh
+    sed -i -e "s|#\? \?ZSH_CUSTOM=.*|ZSH_CUSTOM=${zsh_dir}/omz_custom|g" "$HOME/.zshrc"
 
-	if [ -e "~/.oh-my-zsh/custom/plugins" ]; then
-		if [ ! -e "~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
-			git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-		fi
+    custom_plugins="$HOME/.oh-my-zsh/custom/plugins"
+    [[ -d "$custom_plugins" ]] || return
 
-		if [ ! -e "~/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ] ; then
-			git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-		fi
-	fi
-
+    for plugin in zsh-syntax-highlighting zsh-autosuggestions; do
+        plugin_dir="$custom_plugins/$plugin"
+        [[ -d "$plugin_dir" ]] || git clone "https://github.com/zsh-users/$plugin.git" "$plugin_dir"
+    done
 }
 
+# Initialize systemd services
 init_systemd() {
-	sudo apt install -y xsel
+    sudo apt install -y xsel
+    mkdir -p "$HOME/.config/systemd"
+    stow -d "$dotfiles_dir" -t "$HOME/.config/systemd" systemd
+    systemctl --user daemon-reload
 
-	if [[ ! -e $HOME/.config/systemd ]]; then
-		mkdir $HOME/.config/systemd
-	fi
-
-	stow -d . -t $HOME/.config/systemd systemd
-	systemctl --user daemon-reload
-	cd $systemd_dir
-	for service in $(ls *); do
-		systemctl --user start $service
-		systemctl --user enable $service
-	done
+    for service in "$systemd_dir"/*; do
+        service_name=$(basename "$service")
+        systemctl --user enable --now "$service_name"
+    done
 }
 
+# Install macOS-specific packages
 darwin_specified() {
-	# brew tap mycli
-	brew install symlinks stow node ccls trash vim keepassxc
-	# echo PATH=/opt/homebrew/bin/:$PATH >> ~/.zshrc
-	return
+    brew install symlinks stow node ccls trash vim keepassxc
 }
 
+# Install Linux-specific packages
 linux_specified() {
-	git submodule deinit -f workflows/Ariafred
-
-	if [ "$OS_RELEASE" == "Ubuntu" ]; then
+    git submodule deinit -f workflows/Ariafred
+    if [[ $NAME == "Ubuntu" ]]; then
 		sudo apt update && sudo apt install -y \
 			build-essential \
 			ccls \
@@ -99,78 +93,63 @@ linux_specified() {
 			xclip \
 		       	xsel \
 			zsh
-	fi
+    fi
 
 }
 
+# Platform-specific setup
 system_specified() {
-	if [ "${PLATFORM}" = "linux" ]; then
-		linux_specified
-	fi
-
-	if [ "${PLATFORM}" = "macos" ]; then
-		darwin_specified
-	fi
+    case $PLATFORM in
+        linux) linux_specified ;;
+        macos) darwin_specified ;;
+    esac
 }
 
+# Initialize Git submodules
 init_git() {
-	git submodule update --init --recursive --force --remote
+    git submodule update --init --recursive --force --remote
 }
 
+# Fix broken symlinks
 init_symlinks() {
-	symlinks -d $HOME
+    symlinks -d "$HOME"
 }
 
+# Initialize Vim configuration
 init_vim() {
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	stow -d $conf_dir -t $HOME -R vim
-	vim +PlugUninstal +q
-	vim +PlugInstall +qall 
-	# python3 /home/zhou/.vim/plugged/YouCompleteMe/install.py
-
+    curl -fsLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    stow -d "$conf_dir" -t "$HOME" -R vim
+    vim +PlugUninstall +qall
+    vim +PlugInstall +qall
 }
 
+# Initialize clang-format configuration
 init_clangformat() {
-	stow -d $conf_dir -t $HOME -R clang-format
+    stow -d "$conf_dir" -t "$HOME" -R clang-format
 }
 
+# Main dotfiles initialization
 init_dotfiles() {
-	get_os_release
-	system_specified
-	init_symlinks
+    get_os_release
+    system_specified
+    init_symlinks
 
-	case $1 in
-		init_sh)
-			init_sh
-			;;
-
-		init_conf)
-			init_conf
-			;;
-
-
-		init_zsh_custom_post)
-			init_zsh
-			;;
-
-		vim)
-			init_vim
-			;;
-
-		clang_format)
-			init_clangformat
-			;;
-
-		*)
-			init_git
-			init_symlinks
-			init_sh
-			init_conf
-			init_zsh
-			init_vim
-			;;
-	esac
+    case $1 in
+        init_sh) init_sh ;;
+        init_conf) init_conf ;;
+        init_zsh_custom_post) init_zsh ;;
+        vim) init_vim ;;
+        clang_format) init_clangformat ;;
+        *)  # Default setup
+            init_git
+            init_symlinks
+            init_sh
+            init_conf
+            init_zsh
+            init_vim
+            ;;
+    esac
 }
 
-init_dotfiles $1
+init_dotfiles "$1"
