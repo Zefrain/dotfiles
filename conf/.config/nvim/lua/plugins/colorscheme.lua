771dbedba6c1b31c789262c627cf51e70fdb752a
zefrain <whiothes81@gmail.com>
2024-12-17 16:08:39 +0800

    feat(nvim): change colorscheme

---
+++ conf/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,6 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "Mofiqul/dracula.nvim" },
+  { "projekt0n/github-nvim-theme", name = "github-theme" },
+  vim.cmd.colorscheme("github_dark"),
+}
