814c42147f7bc03ecdfa2285012143f5a5f279f1
zefrain <whiothes81@gmail.com>
2024-12-18 11:30:09 +0800

    chore: update nvim plugins

--- conf/.config/nvim/lua/plugins/cscope.lua
+++ conf/.config/nvim/lua/plugins/cscope.lua
@@ -2,8 +2,8 @@ return {
   "dhananjaylatkar/cscope_maps.nvim",
   dependencies = {
     "nvim-telescope/telescope.nvim", -- optional [for picker="telescope"]
-    "ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
-    "echasnovski/mini.pick", -- optional [for picker="mini-pick"]
+    -- "ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
+    -- "echasnovski/mini.pick", -- optional [for picker="mini-pick"]
   },
 
   config = function()
