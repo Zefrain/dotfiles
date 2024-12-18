d72815dec96da328166fd60c0781d641eb4a292a
zefrain <whiothes81@gmail.com>
2024-12-18 11:07:26 +0800

    chore: remove useless file

--- conf/.config/nvim/lua/config/vim-gutentags.lua
+++ conf/.config/nvim/lua/config/vim-gutentags.lua
@@ -1,6 +1,6 @@
 local group = vim.api.nvim_create_augroup("CscopeBuild", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { "*.c", "*.h" },
+  pattern = { "*.c", "*.h,", "*.cpp" },
   callback = function()
     vim.cmd("Cscope db build")
   end,
