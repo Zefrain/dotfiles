6aa948f8187d5ac54c9a8f15e0a30df8082d32e6
zefrain <whiothes81@gmail.com>
2024-12-18 11:30:09 +0800

    chore: update nvim plugins

---
+++ conf/.config/nvim/lua/plugins/nvim-executor.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "michaelb/sniprun",
+    branch = "master",
+
+    build = "sh install.sh 1",
+    -- do 'sh install.sh 1' if you want to force compile locally
+    -- (instead of fetching a binary from the github release). Requires Rust >= 1.65
+
+    config = function()
+      require("sniprun").setup({
+        -- your options
+      })
+    end,
+  },
+}
