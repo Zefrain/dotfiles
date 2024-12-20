97865fd86c5f254b32e5be39a2475759d4b0f517
zefrain <whiothes81@gmail.com>
2024-12-20 14:58:43 +0800

    chore: optimize scripts via copilot-chat

--- sh/clang_format.sh
+++ sh/clang_format.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-
 err() {
-    echo "[$(date +'%Y-%m-%d %H:%M:%S')]: $@" >&2
+  echo "[$(date +'%Y-%m-%d %H:%M:%S')]: $@" >&2
 }
 fmt() {
-    OLD=$IFS
-    IFS=$'\n'
-    for fname in $@; do
-        clang-format -i -style=google $fname
-    done;
-    IFS=$OLD
+  OLD=$IFS
+  IFS=$'\n'
+  for fname in $@; do
+    clang-format -i -style=google $fname
+  done
+  IFS=$OLD
 }
 flist=$(find ! -name '.*' | egrep -v '\.\w+\/' | egrep '\.(c|cpp|h)$')
 
