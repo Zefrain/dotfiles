3023a8deab8a20752a6e874934525d3e9854af59
zefrain <whiothes81@gmail.com>
2024-12-27 14:21:01 +0800

    chore: make  vim-gutentags support cpp

--- conf/.config/nvim/lua/plugins/vim-gutentags.lua
+++ conf/.config/nvim/lua/plugins/vim-gutentags.lua
@@ -4,7 +4,7 @@ return {
     vim.g.gutentags_modules = { "cscope_maps" } -- This is required. Other config is optional
     vim.g.gutentags_cscope_build_inverted_index_maps = 1
     vim.g.gutentags_cache_dir = vim.fn.expand("~/code/.gutentags")
-    vim.g.gutentags_file_list_command = "fd -e c -e h"
+    vim.g.gutentags_file_list_command = "fd -e c -e h -e cpp"
     -- vim.g.gutentags_trace = 1
   end,
 }
