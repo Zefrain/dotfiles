24ae1018dd99fdf82d25831c619e8d970d273e3b
zefrain <whiothes81@gmail.com>
2025-01-16 17:58:50 +0800

    feat: update nvim config

--- conf/.config/nvim/lua/plugins/lazy_overlap.lua
+++ conf/.config/nvim/lua/plugins/lazy_overlap.lua
@@ -1,3 +1,33 @@
 return {
-    { "Zefrain/friendly-snippets" },
+  -- Modified friendly-snippets
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "Zefrain/friendly-snippets", enabled = true },
+
+  -- Modified grug-far.nvim
+  { "MagicDuck/grug-far.nvim", enabled = false },
+  {
+    "Zefrain/grug-far.nvim",
+    enabled = true,
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          local grug = require("grug-far")
+          -- local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          grug.open({
+            transient = true,
+            prefills = {
+              -- filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+              search = "\\b" .. vim.fn.expand("<cword>") .. "\\b",
+              filesFilter = "*.*",
+              focus = "replace",
+            },
+          })
+        end,
+        mode = { "n", "v" },
+        desc = "Search and Replace",
+      },
+    },
+    opts = { startCursorRow = 3 },
+  },
 }
